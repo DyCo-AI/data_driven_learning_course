de86d5d585ad3c83d272a33caf76924d01fe23ab
Sriram Krishnamoorthy <sriramk@clemson.edu>
2024-10-24 23:57:49 -0400

    renamed folders

--- Assignment 2/dmd/get_DMD.m
+++
@@ -1,9 +0,0 @@
-function DMD = get_DMD(X1,X2)
-% Complete this function to perform Dynamic mode decomposition
-% Inputs
-% X1        : snapshots of data from i = 0 to m-1
-% X2        : sanpshots of data from i = 1 to m
-% Outputs
-% DMD       : n x n Koopman operator
-   
-end
